e6dd53913e6d3fc8bb7281ec4840931683b92625
Ahmed <aaa26770123@gmail.com>
2023-10-06 20:48:47 -0600

    Add testing logic (ip)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import UserCard from "./components/UserCard.jsx";
 
-const RANDOM_USER_GENERATOR_API_URL = 'https://randomuser.me/api/?format=JSON&results=10&nat=CA,US'
+const RANDOM_USER_GENERATOR_API_URL = "https://randomuser.me/api/?format=JSON&results=2&nat=CA,US"
 
 export default function App() {
 
     const [randomUsers, setRandomUsers] = React.useState([])
+    const [isMemorizing, setIsMemorizing] = React.useState(true)
+    const [enteredNames, setEnteredNames] = React.useState([])
 
     React.useEffect(() => {
         fetch(RANDOM_USER_GENERATOR_API_URL)
@@ -15,18 +17,69 @@ export default function App() {
                 setRandomUsers(data.results);
             })
             .catch((error) => {
-                console.error('Error fetching random user data:', error);
+                console.error("Error fetching random user data:", error);
             });
     }, [])
 
     const randomUserElements = randomUsers.map(user =>
         (<UserCard key={user.id.value}
-                   user={user}/>)
+                   user={user}
+                   isMemorizing={isMemorizing}
+                   handleOnChange={handleNameEntered}/>)
     )
 
+    function handleTest() {
+        setIsMemorizing(false)
+    }
+
+    function handleTestSubmit() {
+        let correctCount = 0;
+        let totalUsers = 0;
+
+        // Iterate over randomUsers and check if the entered names match.
+        randomUsers.forEach((randomUser) => {
+            const userId = randomUser.id.value;
+            const enteredUser = enteredNames.find((enteredUser) => enteredUser.id === userId);
+
+            // Check if a user with the same ID exists in enteredNames.
+            if (enteredUser) {
+                totalUsers++;
+
+                // Compare entered name (case-insensitive) with actual name.
+                const isNameCorrect = enteredUser.name.trim().toLowerCase() === randomUser.name.first.toLowerCase()
+                if (isNameCorrect) {
+                    correctCount++;
+                }
+            }
+        });
+
+        // TODO: CLEAN UP; Provide feedback to the player.
+        alert(`You got ${correctCount} names correct out of ${totalUsers}.`);
+
+        setIsMemorizing(true);
+    }
+
+    function handleNameEntered(name, id) {
+        setEnteredNames((prevEnteredNames) => {
+            // Check if the user with the same ID already exists in the array.
+            // If the user exists, update their name, else add a new user to the array.
+            const userIndex = prevEnteredNames.findIndex((user) => user.id === id);
+            if (userIndex !== -1) {
+                prevEnteredNames[userIndex].name = name;
+            } else {
+                prevEnteredNames.push({ id: id, name: name });
+            }
+            return [...prevEnteredNames];
+        });
+    }
+
     return (
-        <div className="user-cards-container">
-            {randomUserElements}
-        </div>
+        <React.Fragment>
+            <div className="user-cards-container">
+                {randomUserElements}
+            </div>
+            {isMemorizing && <button onClick={handleTest}>Test</button>}
+            {!isMemorizing && <button onClick={handleTestSubmit}>Finish Test</button>}
+        </React.Fragment>
     )
 }
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-
 export default function UserCard(props) {
     return (
         <div className="user-card">
-            <img className="user-image" src={props.user.picture.large} alt="User" />
-            <div className="user-name">{props.user.name.first}</div>
+            <img className="user-image" src={props.user.picture.large} alt="User"/>
+            {props.isMemorizing ?
+                <div className="user-name">{props.user.name.first}</div> :
+                <input
+                    type="text"
+                    placeholder="Enter Name"
+                    // value={enteredName}
+                    onChange={(event) => props.handleOnChange(event.target.value, props.user.id.value)}
+                />
+            }
         </div>
     )
 }
