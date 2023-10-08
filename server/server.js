648b73582d409ef1c5ea11518c70a1831c003c87
Ahmed <aaa26770123@gmail.com>
2023-10-08 16:10:18 -0600

    Set up express server (ip)

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-//
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware to parse JSON requests
+// app.use(express.json());
+
+// Sample route for testing
+app.get('/api', (req, res) => {
+    res.json({ message: 'hello hello!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
