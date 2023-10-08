c95e1a27b272d0f4bbc586c8e5b08ee20dca2086
Ahmed <aaa26770123@gmail.com>
2023-10-08 17:39:13 -0600

    Import and use the cors middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 // app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Sample route for testing
 app.get('/api', (req, res) => {
