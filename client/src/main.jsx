d6bdf265aa071968f04493df4c6cc301541dd92e
Ahmed <ahmedhasan8055@gmail.com>
2023-12-15 21:38:52 -0700

    Implement login functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AuthenticationService from "./utils/authentication.js";
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import App from './App.jsx'
 import Login from "./pages/Login.jsx";
 import './index.css'
 
-const isUserAuthenticated = AuthenticationService.isAuthenticated;
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {isUserAuthenticated ? <App/> : <Login/>}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/app" element={<App/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="*" element={<Navigate to="/login"/>}/>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
-)
+);
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import AuthenticationService from "../utils/authentication.js";
 import './login.css'
 
 export default function Login() {
+    const navigate = useNavigate();
     const [isTooltipDisplayed, setIsTooltipDisplayed] = React.useState(false);
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
     const handleTooltipToggle = () => {
         setIsTooltipDisplayed(prevIsTooltipDisplayed => !prevIsTooltipDisplayed);
+    };
+
+    const handleLogin = () => {
+        if (AuthenticationService.login(username, password)) {
+            navigate('/app');
+        }
     };
 
     return (<div className="login-container">
@@ -15,19 +26,25 @@ export default function Login() {
                    className="form-input"
                    placeholder="Username"
                    required
+                   autoComplete="on"
                    onFocus={handleTooltipToggle}
-                   onBlur={handleTooltipToggle}/>
+                   onBlur={handleTooltipToggle}
+                   onChange={(event) => setUsername(event.target.value)}
+            />
             <input type="password"
                    className="form-input"
                    placeholder="Password"
                    required
+                   autoComplete="on"
                    onFocus={handleTooltipToggle}
-                   onBlur={handleTooltipToggle}/>
-            <button type="submit" className="form-submit">Login</button>
+                   onBlur={handleTooltipToggle}
+                   onChange={(event) => setPassword(event.target.value)}
+            />
+            <button onClick={handleLogin} type="submit" className="form-submit">Login</button>
         </form>
         {isTooltipDisplayed && (
             <div className="tooltip">
-                <p>Use "username" for the username and "password" for the password.</p>
+                <p>Use "user" for the username and "password" for the password.</p>
             </div>
         )}
     </div>);
