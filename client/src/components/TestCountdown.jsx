41c3720113203d9fa5ee9d91f45fccf9252b4677
Ahmed <aaa26770123@gmail.com>
2023-12-16 14:28:32 -0700

    Create styled components for TestCountdown

--- client/src/components/TestCountdown.jsx
+++ client/src/components/TestCountdown.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 import {tryFetchData} from "../utils/apiHelper.js";
+import styled from "styled-components";
+
+const TestCountdownContainer = styled.div`
+  text-align: center;
+  padding: 20px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-width: 300px;
+  margin: 0 auto;
+`
+
+const CountdownText = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`
+
+const FunFactText = styled.p`
+  font-size: 16px;
+  margin-top: 10px;
+`
 
 const RANDOM_FACT_API_URL = "https://api.api-ninjas.com/v1/facts?limit=1"
 
@@ -35,10 +57,10 @@ export default function TestCountdown(props) {
     }, [countdownTimeInSeconds]);
 
     return (
-        <div className="test-countdown-container">
-            <h2 className="countdown-text">Recall begins in {countdownTimeInSeconds} seconds</h2>
-            <p className="fun-fact-text">Did you know ... {funFact}</p>
+        <TestCountdownContainer>
+            <CountdownText>Recall begins in {countdownTimeInSeconds} seconds</CountdownText>
+            <FunFactText>Did you know ... {funFact}</FunFactText>
             <button className="skip-button" onClick={props.handleTestCountdown}>Skip</button>
-        </div>
+        </TestCountdownContainer>
     );
 }
