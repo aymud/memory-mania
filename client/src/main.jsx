d248275eb48ed1acc463f107d3c20341d77ec267
Ahmed <ahmedhasan8055@gmail.com>
2023-12-15 18:23:31 -0700

    Create login page template

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AuthenticationService from "./utils/authentication.js";
 import App from './App.jsx'
+import Login from "./pages/Login.jsx";
 import './index.css'
 
+const isUserAuthenticated = AuthenticationService.isAuthenticated;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {isUserAuthenticated ? <App/> : <Login/>}
+    </React.StrictMode>,
 )
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login() {
+
+    return (
+        <React.Fragment>
+            <h1>Login page</h1>
+        </React.Fragment>
+    )
+}
