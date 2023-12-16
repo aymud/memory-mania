0be0dcaef275b8c3b04620199f0877edfeb440aa
Ahmed <aaa26770123@gmail.com>
2023-12-16 15:00:49 -0700

    Update css for login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,6 @@ import styled from "styled-components";
 
 const BodyWrapper = styled.div`
   font-family: 'Arial', sans-serif;
-  background-color: #f4f4f4;
   margin: 0;
   padding: 0;
   display: flex;
