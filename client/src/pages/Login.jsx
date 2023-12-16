fd47f8aad8797d5ec86a59648d175bef4beabe5e
Ahmed <ahmedhasan8055@gmail.com>
2023-12-15 18:44:19 -0700

    Create login page and style it (no functionality)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './login.css'
 
 export default function Login() {
+    const [isTooltipDisplayed, setIsTooltipDisplayed] = React.useState(false);
 
-    return (
-        <React.Fragment>
-            <h1>Login page</h1>
-        </React.Fragment>
-    )
+    const handleTooltipToggle = () => {
+        setIsTooltipDisplayed(prevIsTooltipDisplayed => !prevIsTooltipDisplayed);
+    };
+
+    return (<div className="login-container">
+        <h2>Login</h2>
+        <form className="login-form">
+            <input type="text"
+                   className="form-input"
+                   placeholder="Username"
+                   required
+                   onFocus={handleTooltipToggle}
+                   onBlur={handleTooltipToggle}/>
+            <input type="password"
+                   className="form-input"
+                   placeholder="Password"
+                   required
+                   onFocus={handleTooltipToggle}
+                   onBlur={handleTooltipToggle}/>
+            <button type="submit" className="form-submit">Login</button>
+        </form>
+        {isTooltipDisplayed && (
+            <div className="tooltip">
+                <p>Use "username" for the username and "password" for the password.</p>
+            </div>
+        )}
+    </div>);
 }
