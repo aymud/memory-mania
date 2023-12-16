10f2578619ea0586d63681725c3463dd7e9aa9cf
Ahmed <aaa26770123@gmail.com>
2023-12-16 14:55:05 -0700

    Fix type in private route name

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,14 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import App from './App.jsx'
 import StartMenu from "./components/StartMenu.jsx";
 import Login from "./pages/Login.jsx";
-import PrivateRoutes from "./pages/PrivateRoute.jsx";
+import PrivateRoute from "./pages/PrivateRoute.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route element={<PrivateRoutes/>}>
+                <Route element={<PrivateRoute/>}>
                     <Route element={<App/>} path="/app" exact/>
                 </Route>
                 <Route element={<Login/>} path="/login"/>
--- client/src/pages/PrivateRoute.jsx
+++ client/src/pages/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import { Outlet, Navigate } from 'react-router-dom'
 import AuthenticationService from "../utils/authentication.js";
 
-const PrivateRoutes = () => {
+const PrivateRoute = () => {
     return(
         AuthenticationService.isAuthenticated ? <Outlet/> : <Navigate to="/login"/>
     )
 }
 
-export default PrivateRoutes;
+export default PrivateRoute;
