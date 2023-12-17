edcede56b1659ad17e0ae6a720161cc92653dfb1
Ahmed <aaa26770123@gmail.com>
2023-12-17 08:45:12 -0700

    Fix ESLint rules conflict with Prettier

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
