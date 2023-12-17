f9473f8f3360f30a50598a495e28a4247b9beaf2
Ahmed <aaa26770123@gmail.com>
2023-12-17 10:23:43 -0700

    Add typescript eslint parser

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
+        "plugin:@typescript-eslint/recommended"
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
