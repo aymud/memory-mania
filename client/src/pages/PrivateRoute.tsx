3698e6a872f6245a1fd1f86fc32da13450bf4687
Ahmed <aaa26770123@gmail.com>
2023-12-17 10:33:35 -0700

    Fix errors in PrivateRoute.tsx

--- client/src/pages/PrivateRoute.tsx
+++ client/src/pages/PrivateRoute.tsx
@@ -2,13 +2,10 @@ import { Outlet, Navigate } from 'react-router-dom'
 import AuthenticationService from '../utils/authentication.js'
 
 const PrivateRoute = () => {
-    return (
-        // @ts-ignore
-        AuthenticationService.isAuthenticated ? (
-            <Outlet />
-        ) : (
-            <Navigate to="/login" />
-        )
+    return AuthenticationService.isAuthenticated ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" />
     )
 }
 
