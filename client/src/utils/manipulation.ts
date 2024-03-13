f9c9f40d75acb86a2641c49e1e086c7884d34c07
Ahmed <aaa26770123@gmail.com>
2024-03-13 06:20:47 -0600

    Use generic type for shuffleArray function

--- client/src/utils/manipulation.ts
+++ client/src/utils/manipulation.ts
@@ -1,6 +1,6 @@
 // Shuffle an array randomly using the Fisher-Yates shuffle algorithm.
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export function shuffleArray(array: any[]) {
+export function shuffleArray<T>(array: T[]): T[] {
     const shuffled = [...array];
     for (let i = shuffled.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
