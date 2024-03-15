59b21e1a239ad03b2b53580709ed6c68ad8e1cb7
Ahmed <aaa26770123@gmail.com>
2024-03-15 12:13:40 -0600

    Add render test for Main.tsx

---
+++ client/src/components/tests/Main.test.tsx
@@ -0,0 +1,31 @@
+// @ts-expect-error: React is needed for the App component.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import AuthProvider from '../../context/AuthContext.tsx';
+import PrivateRoute from '../../routes/PrivateRoute.tsx';
+import App from '../../App.tsx';
+import Login from '../../routes/Login.tsx';
+import Profile from '../../routes/Profile.tsx';
+import StartMenu from '../../routes/StartMenu.tsx';
+
+it('renders without crashing', () => {
+    const root: HTMLElement = document.getElementById('root') ?? document.createElement('div');
+
+    ReactDOM.createRoot(root).render(
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route element={<PrivateRoute />}>
+                        <Route element={<App />} path='/app' />
+                    </Route>
+                    <Route element={<Login />} path='/login' />
+                    <Route element={<Profile />} path='/profile' />
+                    <Route element={<StartMenu />} path='*' />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    );
+});
