0380592e889f0175b5b84f703f12341491f1a5d3
Ahmed <aaa26770123@gmail.com>
2024-03-14 23:11:01 -0600

    Fix navbar styling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,26 +2,37 @@ import { FiMenu, FiUser } from 'react-icons/fi';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
+import { useAuthState } from '../hooks/useAuthState.ts';
+
 interface NavbarProps {
     level: number;
 }
 
-const NavbarContainer = styled.div`
-    width: 100%;
-    position: fixed;
-    top: 0;
+const NavbarList = styled.ul`
     display: flex;
-    justify-content: space-between;
     align-items: center;
     padding: 15px;
     background-color: #0b2434;
     color: #fff;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    list-style: none;
+    margin: 0;
+`;
+
+const NavbarItem = styled.li<{ $isRight: boolean }>`
+    padding: 10px;
+    margin-left: 5px;
+    font-size: 1.5em;
+    transition: box-shadow 0.3s;
+    color: #fff;
+
+    ${props => props.$isRight && 'margin-left: auto'};
 `;
 
 const MenuIcon = styled.div`
     font-size: 1.5rem;
     cursor: pointer;
+    color: white;
 `;
 
 const ProfileContainer = styled.div`
@@ -38,34 +49,48 @@ const ProfileIcon = styled.div`
 `;
 
 const ProfileName = styled.div`
-    font-size: 1rem;
+    font-size: 1.5rem;
     color: #fff;
 `;
 
 const LevelInfo = styled.div`
     font-size: 1.5rem;
     font-weight: bold;
+    /* Center the LevelInfo horizontally */
+    flex: 1;
+    text-align: center;
 `;
 
 export default function Navbar(props: NavbarProps) {
     const navigate = useNavigate();
+    const { isAuthenticated, user } = useAuthState();
 
     function handleProfile() {
         navigate('/profile');
     }
 
     return (
-        <NavbarContainer>
-            <MenuIcon>
-                <FiMenu />
-            </MenuIcon>
-            <LevelInfo>Level {props.level}</LevelInfo>
-            <ProfileContainer onClick={handleProfile}>
-                <ProfileIcon>
-                    <FiUser />
-                </ProfileIcon>
-                <ProfileName>User</ProfileName>
-            </ProfileContainer>
-        </NavbarContainer>
+        <header>
+            <nav>
+                <NavbarList>
+                    <MenuIcon>
+                        <FiMenu />
+                    </MenuIcon>
+                    <LevelInfo>Level {props.level}</LevelInfo>
+                    {isAuthenticated ? (
+                        <NavbarItem $isRight>
+                            <ProfileContainer onClick={handleProfile}>
+                                <ProfileIcon>
+                                    <FiUser />
+                                </ProfileIcon>
+                                <ProfileName>{user}</ProfileName>
+                            </ProfileContainer>
+                        </NavbarItem>
+                    ) : (
+                        <NavbarItem $isRight>Login</NavbarItem>
+                    )}
+                </NavbarList>
+            </nav>
+        </header>
     );
 }
