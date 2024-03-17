2f07e1009b3225cda0f54fd862104baaaddfa5e3
Ahmed <aaa26770123@gmail.com>
2024-03-17 15:44:21 -0600

    Cypress config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        istanbul({
+            // Other options...
+            forceBuildInstrumentation: true // Ensure instrumentation during production build
+        })
+    ]
 });
