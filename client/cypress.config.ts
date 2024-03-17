383d2b43e87d2e303496ed1664977e57fe074e1b
Ahmed <aaa26770123@gmail.com>
2024-03-17 16:16:18 -0600

    Cypress config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'cypress';
-import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
+    env: {
+        codeCoverage: {
+            exclude: 'cypress/**/*.*'
+        }
+    },
     e2e: {
-        baseUrl: 'http://localhost:5173/,',
+        baseUrl: 'http://localhost:5173',
         setupNodeEvents(on, config) {
-            codeCoverageTask(on, config);
             return config;
         }
     }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         istanbul({
-            // Other options...
-            forceBuildInstrumentation: true // Ensure instrumentation during production build
+            cypress: true,
+            requireEnv: false
         })
     ]
 });
