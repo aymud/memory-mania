37c5be4688c5e5f364291bf5a7d3ee142edd49fc
Ahmed <aaa26770123@gmail.com>
2024-03-17 00:14:57 -0600

    Update .eslintrc.cjs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -12,9 +12,6 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: './tsconfig.json'
-    },
     settings: { react: { version: '18.2' } },
     plugins: ['react-refresh', 'prettier', 'import'],
     rules: {
