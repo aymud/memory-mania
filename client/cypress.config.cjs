474377e073b1f0af95f573a098b75a937079f13b
Ahmed <37085550+AMax23@users.noreply.github.com>
2024-03-18 14:59:00 -0600

    Add code coverage for jest and cypress tests (#63)
    update packages, add config files, set up code instrumenting, and use github actions to create a pr comment for code coverage

---
+++ client/cypress.config.cjs
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+    env: {
+        codeCoverage: {
+            exclude: 'cypress/**/*.*'
+        }
+    },
+    e2e: {
+        baseUrl: 'http://localhost:5173',
+        setupNodeEvents(on, config) {
+            require('@cypress/code-coverage/task')(on, config);
+
+            on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+
+            return config;
+        }
+    }
+});
