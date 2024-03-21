7544f935855f77511a83b870e5b1abf5f65d17ce
Ahmed <aaa26770123@gmail.com>
2024-03-20 22:43:02 -0600

    Exclude tests from cypress code coverage report

--- client/cypress.config.cjs
+++ client/cypress.config.cjs
@@ -1,11 +1,6 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    env: {
-        codeCoverage: {
-            exclude: ['cypress/**/*.*', 'src/**/*.test.*']
-        }
-    },
     e2e: {
         baseUrl: 'http://localhost:5173',
         setupNodeEvents(on, config) {
